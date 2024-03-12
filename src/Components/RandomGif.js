c3562c69c72e671e1123ef9d3cde74228d0d622f
aman-sharma-as <aman.745500@gmail.com>
2024-03-12 16:01:53 +0530

    Commenting useEffect & useState

--- src/Components/RandomGif.js
+++ src/Components/RandomGif.js
@@ -1,41 +1,45 @@
-import {React, useEffect, useState} from 'react';
-import axios from 'axios';
-import Spinners from './Spinners';
-import useGif from '../hooks/useGif';
-
+// import {React, useEffect, useState} from 'react';
+// import axios from 'axios';
+import Spinners from "./Spinners";
+import useGif from "../hooks/useGif";
 
 // const API_KEY= process.env.REACT_APP_GIPHY_API_KEY;
 
 const RandomGif = () => {
-//     const [gif, setGif]=useState('');
-//     const[loading, setLoading] = useState('false');
-   
-    // async function fetchData(){
-    //     setLoading(true);
-    //     const url= `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    //     const {data}=await axios.get(url);
-    //     const imagesource=data.data.images.downsized_large.url;
-    //     setGif(imagesource);
-    //     setLoading(false);
+  //     const [gif, setGif]=useState('');
+  //     const[loading, setLoading] = useState('false');
 
-    // }
-    // useEffect(()=>{
-    //     fetchData()
-    // },[]);
+  // async function fetchData(){
+  //     setLoading(true);
+  //     const url= `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+  //     const {data}=await axios.get(url);
+  //     const imagesource=data.data.images.downsized_large.url;
+  //     setGif(imagesource);
+  //     setLoading(false);
 
-    const {gif, loading, fetchData} = useGif();
-  
+  // }
+  // useEffect(()=>{
+  //     fetchData()
+  // },[]);
+
+  const { gif, loading, fetchData } = useGif();
+
   return (
-    
-    <div className='flex flex-col items-center gap-y-5 w-1/2 bg-green-500 rounded-lg border border-black'>
-        <h1 className='mt-[15px] text-2xl underline uppercase font-bold'>A Random Gif</h1>
-        {
-            loading ? (<Spinners/>) : (<img src={gif} width="450"/>)
-        }
-        
-        <button className='w-10/12 bg-white opacity-80 rounded-md py-2 mb-[20px]' onClick={() => fetchData()}> Generate</button>
-    </div>
-  )
-}
+    <div className="flex flex-col items-center gap-y-5 w-1/2 bg-green-500 rounded-lg border border-black">
+      <h1 className="mt-[15px] text-2xl underline uppercase font-bold">
+        A Random Gif
+      </h1>
+      {loading ? <Spinners /> : <img src={gif} width="450" />}
 
-export default RandomGif
+      <button
+        className="w-10/12 bg-white opacity-80 rounded-md py-2 mb-[20px]"
+        onClick={() => fetchData()}
+      >
+        {" "}
+        Generate
+      </button>
+    </div>
+  );
+};
+
+export default RandomGif;
